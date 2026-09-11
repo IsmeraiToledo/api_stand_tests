e5e295f9d9a5317b9388b51e54afb66d6a75ac68
Ismerai Toledo <iaremsi28@hotmail.com>
2026-09-11 16:46:00 -0500

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-3092e242-6d7a-4072-8e43-879590817f89.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/" #endpoint
+LOG_MAIN_PATH = "/api/logs/main/" #endpoint2
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
